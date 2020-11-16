3547bcdcde36e61522e8e85f75aed3cfefac3251
rashikapr <74289013+rashikapr@users.noreply.github.com>
2020-11-16 10:59:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+function draw(){
+  background(0);
+   
+    
+    drawSprites();
+  
+   
+
+}
+
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+   
+}
+
